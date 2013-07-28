de0e760eda09cd2c00bb7ba6374e5771bb5e933d
Luisa Lima <luisalima@dcc.fc.up.pt>
2013-07-28 17:54:13 +0100

    Add missing "-y".

--- provision_scripts/prepare_project_dependencies.sh
+++ provision_scripts/prepare_project_dependencies.sh
@@ -6,7 +6,7 @@ source $DIR/../config/all.sh
 DEPENDENCIES="nodejs postgresql sqlite3 libsqlite3-dev libxml2 build-essential libxslt-dev libxml2-dev xvfb firefox"
 
 sudo apt-get update
-sudo apt-get install python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y $DEPENDENCIES
