ef56a9c9e20bbe74f366f798440767e95c2fc945
Luisa Lima <luisalima@dcc.fc.up.pt>
2013-07-28 19:44:52 +0100

    Clean up code.

--- config/public.sh
+++ config/public.sh
@@ -15,3 +15,19 @@ CI_SCRIPTS=$ROOT_DIR/ci_scripts
 
 RUBIES="2.0.0 1.9.3"
 DEFAULT_RUBY="2.0.0"
+
+function display_task {
+    lightblue='\e[0;36m'
+    nocolor='\e[0m'
+    echo -e "${lightblue}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ">>>>>> $1 ..........."
+    echo -e "${nocolor}"
+}
+
+function sudo_cmd {
+    green='\e[0;32m'
+    nocolor='\e[0m'
+    echo -e "${green}[$(date)] $1 ${nocolor}"
+    sudo $1
+}
