c1fbacbbb54605b788db1639518699b185ce0524
Luisa Lima <luisalima@dcc.fc.up.pt>
2013-07-28 20:08:30 +0100

    Load backup automatically from JENKINS_BACKUP directory.

--- provision_scripts/install_jenkins_and_plugins.sh
+++ provision_scripts/install_jenkins_and_plugins.sh
@@ -9,9 +9,14 @@ if [ -z "$(grep jenkins /etc/apt/sources.list)" ]
     sudo bash -c "echo deb http://pkg.jenkins-ci.org/debian binary/ >> /etc/apt/sources.list"
     echo "Added jenkins to aptitude sources list"
 fi
-sudo apt-get update
-sudo apt-get install -y jenkins
-sudo usermod -a -G shadow jenkins
+sudo_cmd apt-get update
+sudo_cmd apt-get install -y jenkins
+sudo_cmd usermod -a -G shadow jenkins
+
+if [ -d $JENKINS_BACKUP ] then
+    echo "copying jenkins backup from $JENKINS_BACKUP..."
+    sudo_cmd cp -r $JENKINS_BACKUP/* ~jenkins/
+fi
 
 for PLUGIN in $JENKINS_PLUGINS
   do
