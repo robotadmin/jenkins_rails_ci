36231b767ed9edd693ca92e07ee9631e64a40854
Luisa Lima <luisalima@dcc.fc.up.pt>
2013-07-29 13:32:42 +0100

    Add libpq-dev, necessary for pg gem.

--- provision_scripts/prepare_project_dependencies.sh
+++ provision_scripts/prepare_project_dependencies.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo ------ $DIR ------
 source $DIR/../config/all.sh
 
-DEPENDENCIES="nodejs postgresql sqlite3 libsqlite3-dev libxml2 build-essential libxslt-dev libxml2-dev xvfb firefox"
+DEPENDENCIES="nodejs postgresql libpq-dev sqlite3 libsqlite3-dev libxml2 build-essential libxslt-dev libxml2-dev xvfb firefox"
 
 sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
